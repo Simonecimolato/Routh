5eb862bd56eb6e0b01bd5f4296dc3e1adccf12df
Simone Cimolato <Simonecimolato@users.noreply.github.com>
2022-04-18 02:14:34 +0200

    Update routh.m

--- routh.m
+++ routh.m
@@ -6,7 +6,7 @@ clear
 clc
 
 %%  INPUT DATA
-coeffs = [8 10 21 4 9]; % in order, include 0s
+coeffs = [1 3 4 2]; % in order, include 0s, [1 2 2 4 1 2] says it's stable but it's not, it has repeated poles on the imaginari axis
 
 %%  ROUTH TABLE CONSTRUCTION
 R = zeros(length(coeffs)-1+1,ceil((length(coeffs)-1+1)/2)+1);
@@ -27,18 +27,10 @@ R(:,size(R,2)+1) = zeros(size(R,1),1);
 
 for i = 3:size(R,1)
     for j = 2:size(R,2)-1
-        %routh table formula
-        R(i,j) = (-1/R(i-1,2)) * det([R(i-2,2) R(i-2,j+1); R(i-1,2) R(i-1,j+1)]);
-        %correcting first row zeros
-        if R(i,2) == 0
-            k = 2;
-            %if the row is all zeros skip correction
-            while R(i,k) == 0 && k < length(R(i,:))
-                k = k+1;
-            end
-            if k ~= length(R(i,:)) 
-                R(i,2) = R(i,k)*((-1)^(k-1));
-            end
+        R(i,j) = (-1/R(i-1,2)) * det([R(i-2,2) R(i-2,j+1); R(i-1,2) R(i-1,j+1)]); %routh table formula
+        if any(R(i,2:end))         %correcting first row zeros, if the row is all zeros skip correction
+            pos = find(R(i,2:end));
+            R(i,2) = R(i,pos(1)+1)*((-1)^pos(1));
         end
     end
 end
@@ -46,8 +38,7 @@ end
 %removing first and last column 
 R = R(1:end,2:end-1);
 
-%checking if a row is NaN or all zeros and only saving previous rows in
-%Routh table
+%checking if a row is all zeros or first element is NaN and only saving previous rows in Routh table
 for i = 1:size(R,1)
     if isnan(R(i,1)) || not(any(R(i,:)))
         R = R(1:(i-1),:);
@@ -59,8 +50,7 @@ end
 disp('Routh table: ')
 disp(R);
 
-%checking if the system is stable or not by checking the sign of the first
-%column
+%checking if the system is stable by checking the sign of the first column
 if any(diff(sign(R(:,1).*(R(:,1)~=0))))
     disp('Unstable system')
 else
